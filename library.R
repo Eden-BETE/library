e4d9a9421a8181bad10c68a5524e9a041b40af3f
Théo DENIS <theo.denis3.etu@univ-lille.fr>
2025-05-11 17:14:08 +0200

    init template

--- library.R
+++ library.R
@@ -10,6 +10,8 @@ library(shinymanager)
 library(DT)
 library(reticulate)
 library(rstudioapi)
+library(openxlsx)
+library(readxl)
 
 
 # ==============================================================================
@@ -24,3 +26,88 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # ==============================================================================
 
 
+
+# ==============================================================================
+#                                  Application
+# ==============================================================================
+
+ui <- dashboardPage(
+  skin = "yellow",
+  
+  dashboardHeader(title = "Library"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Bibliothèque", tabName = "bibliotheque", icon = icon("user")),
+      menuItem("Statistiques", tabName = "statistiques", icon = icon("search")),
+      menuItem("Spinner Wheel", tabName = "spinner_wheel", icon = icon("search"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+    
+# ------------------------------------------------------------------------------
+#                                    Page 1
+# ------------------------------------------------------------------------------
+    
+      tabItem(
+        tabName = "bibliotheque",
+        fluidRow(
+          box(
+            title = "Importer la bibliothèque",
+            status = "primary",
+            width = 12,
+            solidHeader = TRUE,
+            fluidRow(
+              fileInput("library_csv", "Choisissez votre bibliothèque", accept = c(".xlsx", ".csv")),
+              br(), br(),
+              DTOutput("table_data", width = "100%")
+            )
+          )
+        )
+        
+        
+      ),
+
+# ------------------------------------------------------------------------------
+#                                    Page 2
+# ------------------------------------------------------------------------------
+
+      tabItem(
+        tabName = "statistiques",
+        fluidRow(
+          box(
+            title = "Statistiques",
+            status = "primary",
+            width = 12,
+            solidHeader = TRUE
+            
+          )
+        )
+      ),
+
+# ------------------------------------------------------------------------------
+#                                    Page 3
+# ------------------------------------------------------------------------------
+
+      tabItem(
+        tabName = "spinner_wheel"
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$table_data <- renderDT({
+    req(input$library_csv)
+    
+    df = read_xlsx(input$library_csv$datapath, col_names = TRUE)
+    
+    datatable(df, options = list(scrollX = TRUE, pageLength = 10), rownames = FALSE)
+  })
+  
+}
+
+
+
+shinyApp(ui, server)
